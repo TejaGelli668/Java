62cff2aeae380d7dc22cd628c400c209926033cf
TejaGelli <126828441+TejaGelli@users.noreply.github.com>
2024-02-14 10:45:57 -0600

    ReverseNumbers.java

---
+++ ReverseNumbers.java
@@ -0,0 +1,8 @@
+public class ReverseNumbers {
+    public static void main(String [] args){
+        System.out.println("Reverse Numbers from 10 - 1");
+        for(int i =10; i>0; i--){
+            System.out.print(i + " ");
+        }
+    }
+}
